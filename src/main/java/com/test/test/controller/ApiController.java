1982d04951bad5c5d29463abdf0439c91a024427
nsotechnix <ns.otechnix@gmail.com>
2022-03-03 12:56:07 +0530

    initial commit

---
+++ src/main/java/com/test/test/controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.test.test.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("/api/health")
+    public ResponseEntity<?> healthcheck(){
+        return  ResponseEntity.ok("Works successfully");
+    }
+}
